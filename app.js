b79f5cb11b19ea6bf5c8e51aad718e877872d278
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-08-24 23:40:47 +0300

    i lost my all commits :(

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 
     const config = {
         sideBtnText: 'Get Your Gift',
+        closeIcon: 'X',
+        prevIcon: '⬅',
+        storageKey: 'ins-gameWon',
+        cacheExpiry: 86400000,
         gameAssets: {
             cardCover: 'https://raw.githubusercontent.com/irfansubasi/card-matching-campaign/refs/heads/main/pics%20for%20url/back.png',
             cardList: [
@@ -13,19 +17,40 @@
             backgroundImage: 'https://raw.githubusercontent.com/irfansubasi/card-matching-campaign/refs/heads/main/pics%20for%20url/bg.png',
         },
         pageText: {
-            page_0: {
-                title: 'Match Card and Win The Prize!',
+            startPage: {
+                title: 'Match Cards and Win The Prize!',
                 description: 'For the 10th anniversary of the Great Rock/Metal festival, play the card matching game and get your gift!',
                 gift: '40% discount on festival ticket!',
                 buttonText: 'Start',
             },
-
+            gamePage: {
+                title: 'Match Cards!',
+                shuffleBtn: 'Shuffle!',
+                readyBtn: "I'm Ready!",
+            },
+            resultPage: {
+                win: {
+                    title: 'You Win!',
+                    description: 'Copy the coupon code below and get ready to headbang!',
+                    warning: 'You can use this coupon code only once. The coupon code is valid for a single ticket on a single account.',
+                    buttonText: 'Copy',
+                    couponCode: 'RIPOZZY25',
+                },
+                lose: {
+                    title: 'You Lose!',
+                    description: 'Catch the rock spirit. You should try again.',
+                    buttonText: 'Try Again',
+                }
+            }
         },
         offEvents: [
             '.openModal',
             '.closeModal',
         ]
     }
+
+    let gameStarted = false;
+    let pair = [];
 
     const classes = {
         style: 'ins-custom-style',
@@ -34,7 +59,10 @@
         sideBtnText: 'ins-side-btn__text',
         modalOverlay: 'ins-modal-overlay',
         modal: 'ins-modal',
+        closeBtn: 'ins-close-btn',
+        prevBtn: 'ins-prev-btn',
         show: 'ins-show',
+        hide: 'ins-hide',
         slider: 'ins-slider',
         track: 'ins-slider__track',
         page: 'ins-slider__page',
@@ -42,6 +70,14 @@
         description: 'ins-slider__description',
         gift: 'ins-slider__gift',
         startBtn: 'ins-start-btn',
+        cardList: 'ins-card-list',
+        cardItem: 'ins-card-item',
+        shuffleBtn: 'ins-shuffle-btn',
+        readyBtn: 'ins-ready-btn',
+        flipCardAnimation: 'ins-flip-card-animation',
+        shuffleAnimation: 'ins-shuffle-animation',
+        opened: 'ins-opened-card',
+        tryAgainBtn: 'ins-tryagain-btn',
     }
 
     const selectors = Object.keys(classes).reduce((createdSelector, key) => (
@@ -61,7 +97,7 @@
     };
 
     self.reset = () => {
-        const { style } = selectors;
+        const { style } = selectors; //virgülle ayırabilirsin removeda
 
         $(style).remove();
 
@@ -83,7 +119,7 @@
 
     self.buildCSS = () => {
         const { style } = classes;
-        const { wrapper, sideBtn, sideBtnText, modal, modalOverlay, show, slider, track, page, title, description, gift, startBtn } = selectors;
+        const { wrapper, sideBtn, sideBtnText, modal, modalOverlay, show, hide, slider, track, page, title, description, gift, startBtn, cardItem, cardList, shuffleBtn, readyBtn, flipCardAnimation, shuffleAnimation, tryAgainBtn, closeBtn, prevBtn } = selectors;
 
         const customStyle = `
             <style class="${style}">
@@ -127,26 +163,28 @@
                     background-color: rgba(0, 0, 0, 0.5);
                 }
 
+                ${modal} *{
+                    margin: 0;
+                    padding: 0;
+                    box-sizing: border-box;
+                }
+
                 ${modal}{
                     font-family: 'Verdana', sans-serif;
                     position: fixed;
                     top: 50%;
                     left: 50%;
                     transform: translate(-50%, -50%);
-                    width: 600px;
-                    height: 450px;
+                    width: 800px;
+                    height: 600px;
                     background: url(${config.gameAssets.backgroundImage})center center;
                     border-radius: 30px;
                     border: 2px solid #ffffff;
                 }
 
-                ${modalOverlay}${show}{
-                    display: block;
-                }
-
                 ${slider}{
-                    width: 600px;
-                    height: 450px;
+                    width: 100%;
+                    height: 100%;
                     display: flex;
                     transition: transform 0.5s ease-in-out;
                 }
@@ -154,6 +192,30 @@
                 ${track}{
                     display: flex;
                     transition: transform 0.5s ease-in-out;
+                }
+
+                ${prevBtn}{
+                    font-size: 24px;
+                    font-weight: 700;
+                    text-transform: uppercase;
+                    cursor: pointer;
+                    color: #ffffff;
+                    position: absolute;
+                    top: 20px;
+                    left: 20px;
+                    z-index: 20;
+                }
+
+                ${closeBtn}{
+                    font-size: 24px;
+                    font-weight: 700;
+                    text-transform: uppercase;
+                    cursor: pointer;
+                    position: absolute;
+                    top: 20px;
+                    right: 20px;
+                    color: #ffffff;
+                    z-index: 20;
                 }
 
                 ${page}{
@@ -166,7 +228,11 @@
                     justify-content: space-around;
                     color: #ffffff;
                     text-align: center;
-                    padding: 50px;
+                    padding: 30px;
+                }
+
+                ${page}:nth-child(2) {
+                    position: relative;
                 }
 
                 ${title}{
@@ -185,7 +251,7 @@
                     font-weight: 700;
                 }
 
-                ${startBtn}{
+                ${startBtn}, ${shuffleBtn}, ${readyBtn}, ${tryAgainBtn}{
                     font-size: 20px;
                     font-weight: 700;
                     text-transform: uppercase;
@@ -195,6 +261,85 @@
                     cursor: pointer;
                     padding: 10px 5px;
                 }
+
+                ${cardList} {
+                    display: grid;
+                    grid-template-columns: repeat(3, 1fr);
+                    gap: 16px;
+                    width: 100%;
+                }
+                
+                ${cardItem} {
+                    max-width: 160px;
+                    width: 100%;
+                    aspect-ratio: 3 / 4;
+                    overflow: hidden;
+                    border-radius: 8px;
+                    justify-self: center;
+                }
+                
+                ${cardItem} img {
+                    width: 100%;
+                    height: 100%;
+                    object-fit: cover;
+                }
+                
+                ${readyBtn}{
+                    display:none;
+                }
+
+                ${flipCardAnimation}{
+                    animation: cardFlip 1s ease-in-out;
+                }
+
+                ${shuffleAnimation}{
+                    animation: shuffleMove 3s ease-in-out;
+                }
+
+                ${show}{
+                    display: block;
+                }
+
+                ${hide}{
+                    display: none;
+                }
+
+                @keyframes cardFlip {
+                    0% {
+                        transform: rotateY(0deg);
+                    }
+
+                    50% {
+                        transform: rotateY(90deg);
+                    }
+
+                    100% {
+                        transform: rotateY(0deg);
+                    }
+                }
+
+                @keyframes shuffleMove {
+                    0% {
+                        transform: translateX(0);
+                    }
+
+                    25% {
+                        transform: translateX(-50px);
+                    }
+
+                    50% {
+                        transform: translateX(1000px);
+                    }
+
+                    75% {
+                        transform: translateX(1000px);
+                    }
+
+                    100% {
+                        transform: translateX(0);
+                    }
+                }
+
             </style>
         `
 
@@ -202,7 +347,7 @@
     }
 
     self.buildHTML = () => {
-        const { sideBtn, wrapper, sideBtnText, modal, modalOverlay, slider, track, page, title, description, gift, startBtn } = classes;
+        const { sideBtn, wrapper, sideBtnText, modal, modalOverlay, slider, track, page, title, description, gift, startBtn, shuffleBtn, readyBtn, cardItem, cardList, closeBtn, prevBtn } = classes;
         const { modal: modalSelector } = selectors;
 
         const html = `
@@ -221,23 +366,34 @@
         `;
 
         const sliderHTML = `
+            <div class="${closeBtn}">${config.closeIcon}</div>
             <div class="${slider}">
                 <div class="${track}">
                     <div class="${page}">
-                        <h2 class="${title}">${config.pageText.page_0.title}</h2>
-                        <p class="${description}">${config.pageText.page_0.description}</p>
-                        <p class="${gift}">${config.pageText.page_0.gift}</p>
-                        <div class="${startBtn}">${config.pageText.page_0.buttonText}</div>
+                        <h2 class="${title}">${config.pageText.startPage.title}</h2>
+                        <p class="${description}">${config.pageText.startPage.description}</p>
+                        <p class="${gift}">${config.pageText.startPage.gift}</p>
+                        <div class="${startBtn}">${config.pageText.startPage.buttonText}</div>
                     </div>
                     <div class="${page}">
-                        <p>page1 test</p>
+                        <div class="${prevBtn}">${config.prevIcon}</div>
+                        <h2 class="${title}">${config.pageText.gamePage.title}</h2>
+                        <div class="${cardList}">
+                            ${config.gameAssets.cardList.map(card => `
+                                <div class="${cardItem}">
+                                    <img src="${card}" alt="card">
+                                </div>
+                            `).join('').repeat(2)}
+                        </div>
+                        <div class="${shuffleBtn}">${config.pageText.gamePage.shuffleBtn}</div>
+                        <div class="${readyBtn}">${config.pageText.gamePage.readyBtn}</div>
                     </div>
                     <div class="${page}">
-                        <p>page2 text</p>
                     </div>
                 </div>
             </div>
         `;
+
 
         $(html).appendTo('body');
         $(modalHTML).appendTo('body');
@@ -245,16 +401,21 @@
     }
 
     self.setEvents = () => {
-        const { show, startBtn } = classes;
-        const { sideBtn, modalOverlay, modal } = selectors;
+        const { show } = classes;
+        const { sideBtn, modalOverlay, shuffleBtn, cardItem, readyBtn, tryAgainBtn, closeBtn, startBtn, prevBtn, couponCode } = selectors;
 
-        $(document).on('click.changePage', modal, (event) => {
-            if (event.target.classList.contains(startBtn)) {
-                self.slideTo(1);
-            }
+        $(document).on('click.changePage', startBtn, () => {
+            self.slideTo(1);
         });
 
         $(document).on('click.openModal', sideBtn, () => {
+            const hasPlayed = self.getLocalStorage();
+
+            if (hasPlayed) {
+                alert('You have already played this game.');
+                return;
+            }
+
             $(modalOverlay).toggleClass(show);
         });
 
@@ -263,11 +424,216 @@
                 $(modalOverlay).toggleClass(show);
             }
         });
+
+        $(document).on('click.closeModal', closeBtn, (event) => {
+            if (event.target === event.currentTarget) {
+                $(modalOverlay).toggleClass(show);
+            }
+        });
+
+        $(document).on('click.prevPage', prevBtn, self.resetGame);
+
+        $(document).on('click.shuffleCards', shuffleBtn, self.shuffleCards);
+
+        $(document).on('click.gameStart', readyBtn, self.startGame);
+
+        $(document).on('click.cardGame', cardItem, self.handleCardClick);
+
+        $(document).on('click.restartGame', tryAgainBtn, self.resetGame);
+
+        $(document).on('click.copyCouponCode', couponCode, self.copyCouponCode);
+
     }
 
-    self.slideTo = (page) => { $(selectors.slider).css('transform', `translateX(-${page * 600}px)`) };
+    self.shuffleDeck = () => {
+        const { cardItem } = selectors;
 
+        let allCards = config.gameAssets.cardList.concat(config.gameAssets.cardList);
 
+        //fisher-yates
+        for (let i = allCards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [allCards[i], allCards[j]] = [allCards[j], allCards[i]];
+        }
 
-    setTimeout(self.init, 2.5 * 1000);
+        const cards = $(cardItem).find('img');
+        cards.each((index, img) => {
+            $(img).attr('data-src', allCards[index]);
+        });
+
+    }
+
+    self.updateShuffleUI = () => {
+        const { shuffleBtn, readyBtn, cardItem } = selectors;
+        const { hide, show, flipCardAnimation, shuffleAnimation } = classes;
+
+        $(shuffleBtn).removeClass(show).addClass(hide);
+        $(cardItem).addClass(flipCardAnimation);
+
+        setTimeout(() => {
+            $(cardItem).find('img').attr('src', config.gameAssets.cardCover);
+        }, 500);
+
+        setTimeout(() => {
+            $(cardItem).addClass(shuffleAnimation);
+        }, 1000);
+
+        setTimeout(() => {
+            $(readyBtn).removeClass(hide).addClass(show);
+            $(cardItem).removeClass(flipCardAnimation);
+            $(cardItem).removeClass(shuffleAnimation);
+        }, 4000);
+    }
+
+    self.shuffleCards = () => {
+        self.shuffleDeck();
+        self.updateShuffleUI();
+    };
+
+    self.handleCardClick = (event) => {
+
+        const { opened } = classes;
+        const { cardItem, opened: openedSelector } = selectors;
+
+        if (!gameStarted) return;
+
+        const selectedCard = $(event.currentTarget);
+        const cardImg = selectedCard.find('img').attr('data-src');
+
+        if (selectedCard.hasClass(opened)) return;
+
+        selectedCard.addClass(opened);
+
+        selectedCard.find('img').attr('src', cardImg);
+
+        if (pair.length < 2) {
+            pair.push(cardImg);
+        }
+
+        if (pair.length === 2) {
+            if (pair[0] == pair[1]) {
+                pair = [];
+
+                const totalCards = $(cardItem).length;
+                const openedCards = $(cardItem).filter(openedSelector).length;
+
+                if (openedCards === totalCards) {
+                    self.updateResultGame(true);
+                    self.setLocalStorage();
+                }
+            } else {
+                self.updateResultGame(false);
+            }
+        }
+
+    }
+
+    self.startGame = () => {
+        const { readyBtn, cardItem } = selectors;
+        const { hide, flipCardAnimation } = classes;
+
+        $(readyBtn).toggleClass(hide);
+        $(cardItem).addClass(flipCardAnimation);
+
+        setTimeout(() => {
+            $(cardItem).find('img').each((index, img) => {
+                $(img).attr('src', $(img).attr('data-src'));
+            });
+        }, 500);
+
+        setTimeout(() => {
+            $(cardItem).removeClass(flipCardAnimation);
+            setTimeout(() => {
+                $(cardItem).addClass(flipCardAnimation);
+                setTimeout(() => {
+                    gameStarted = true;
+                    $(cardItem).find('img').attr('src', config.gameAssets.cardCover);
+                }, 500);
+            }, 500);
+        }, 4000);
+    }
+
+    self.resetGame = () => {
+        const { shuffleBtn, readyBtn, cardItem } = selectors;
+        const { show, hide, opened, flipCardAnimation, shuffleAnimation } = classes;
+
+        gameStarted = false;
+        pair = [];
+
+        $(cardItem).find('img').each((index, img) => {
+            $(img).attr('src', config.gameAssets.cardList[Math.floor(index / 2)]);
+            $(img).removeAttr('data-src');
+        });
+
+        $(cardItem).removeClass(opened);
+        $(cardItem).removeClass(flipCardAnimation);
+        $(cardItem).removeClass(shuffleAnimation);
+        $(shuffleBtn).removeClass(hide).addClass(show);
+        $(readyBtn).removeClass(show).addClass(hide);
+
+        self.slideTo(0);
+    };
+
+    self.updateResultGame = (isWin) => {
+        const { win, lose } = config.pageText.resultPage;
+        const { track, page } = selectors;
+        const { tryAgainBtn, title, description } = classes;
+
+        const winHTML = `
+            <h2 class="${title}">${win.title}</h2>
+            <div class="${description}">${win.description}</div>
+            <div>
+                <div>${win.couponCode}</div>
+                <div>${win.buttonText}</div>
+            </div>
+            <div>${win.warning}</div>
+        `;
+
+        const loseHTML = `
+            <h2 class="${title}">${lose.title}</h2>
+            <div class="${description}">${lose.description}</div>
+            <div class="${tryAgainBtn}">${lose.buttonText}</div>
+        `;
+
+        const resultHTML = isWin ? winHTML : loseHTML;
+
+        $(track).find(page).last().html(resultHTML);
+
+        self.slideTo(2);
+    }
+
+    self.copyCouponCode = () => {
+        const { couponCode } = config.pageText.resultPage.win;
+        navigator.clipboard.writeText(couponCode);
+    }
+
+    self.setLocalStorage = (expiry = config.cacheExpiry) => {
+        const toStore = {
+            value: 'true',
+            expiry: Date.now() + expiry,
+        }
+
+        localStorage.setItem(config.storageKey, JSON.stringify(toStore));
+    }
+
+    self.getLocalStorage = () => {
+        const stored = localStorage.getItem(config.storageKey);
+
+        if (!stored) return null;
+
+        const parsed = JSON.parse(stored);
+
+        if (!parsed.expiry || !parsed.expiry) return null;
+
+        if (Date.now() > parsed.expiry) {
+            localStorage.removeItem(config.storageKey);
+            return null;
+        }
+
+        return parsed.value;
+    }
+
+    self.slideTo = (page) => { $(selectors.slider).css('transform', `translateX(-${page * 800}px)`) };
+
+    self.init();
 })({})
